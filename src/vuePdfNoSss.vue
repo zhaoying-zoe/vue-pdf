c30dc4e715b26b05f60dda58d3ab7df8f744fdbf
zhaoying <1130051785@qq.com>
2022-09-06 20:01:29 +0800

    修复qiankun无法使用vue-pdf的bug

--- src/vuePdfNoSss.vue
+++ src/vuePdfNoSss.vue
@@ -1,16 +1,16 @@
 <style src="./annotationLayer.css"></style>
-<script>
 
+<script>
 	import componentFactory from './componentFactory.js'
 
 	if ( process.env.VUE_ENV !== 'server' ) {
-
 		var pdfjsWrapper = require('./pdfjsWrapper.js').default;
 		var PDFJS = require('pdfjs-dist/es5/build/pdf.js');
 
 		if ( typeof window !== 'undefined' && 'Worker' in window && navigator.appVersion.indexOf('MSIE 10') === -1 ) {
-
-			var PdfjsWorker = require('worker-loader!pdfjs-dist/es5/build/pdf.worker.js');
+            // 注释原本的引入方法
+			// var PdfjsWorker = require('worker-loader!pdfjs-dist/es5/build/pdf.worker.js');
+            var PdfjsWorker=require('pdfjs-dist/es5/build/pdf.worker.js');
 			PDFJS.GlobalWorkerOptions.workerPort = new PdfjsWorker();
 		}
 
